5c0f06bf8482de5c48aa6e9bfeca69da2c31209e
cnlimiter <cnlimiter@163.com>
2022-04-12 13:51:04 +0800

    Initial forge-1.18.2-1.0.0

---
+++ src/main/java/nova/committee/moreleads/MoreLeads.java
@@ -0,0 +1,24 @@
+package nova.committee.moreleads;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Static.MOD_ID)
+public class MoreLeads {
+
+    // Directly reference a log4j logger.
+
+    public MoreLeads() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+
+}
