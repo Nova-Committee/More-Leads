b2e6380626d12defd57e2cfb7b9f2cca9e186ea6
cnlimiter <cnlimiter@163.com>
2022-04-13 13:07:07 +0800

    Initial fabric-1.18.2-1.0.0

---
+++ src/main/java/nova/committee/moreleads/Moreleads.java
@@ -0,0 +1,16 @@
+package nova.committee.moreleads;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Description:
+ * Author: cnlimiter
+ * Date: 2022/4/13 12:53
+ * Version: 1.0
+ */
+public class Moreleads implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
