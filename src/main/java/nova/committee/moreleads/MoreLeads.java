a71ca5ada31da4210f616ddb04fcf48819c0307d
cnlimiter <cnlimiter@163.com>
2022-09-03 19:29:55 +0800

    update to 1.19.2

--- src/main/java/nova/committee/moreleads/MoreLeads.java
+++ src/main/java/nova/committee/moreleads/MoreLeads.java
@@ -6,11 +6,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import nova.committee.moreleads.init.ModConfig;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Static.MOD_ID)
 public class MoreLeads {
-
-    // Directly reference a log4j logger.
 
     public MoreLeads() {
         ModConfig.init();
