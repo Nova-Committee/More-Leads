e441e2efc5752832b86df131ffb805cb960cd5c8
cnlimiter <cnlimiter@163.com>
2024-09-24 21:04:54 +0800

    feat(neoforge): add support for NeoForge loader
    Add support in the build configuration for the NeoForge loader by integrating the
     NeoForge plugin and adjusting the project structure to accommodate NeoForge-specific
     configuration files. This change enables the mod to be compatible with the NeoForge
     environment while maintaining existing loader support.

---
+++ neoforge/src/main/java/committee/nova/mods/moreleads/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,34 @@
+package committee.nova.mods.moreleads.platform;
+
+import committee.nova.mods.moreleads.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+
+    @Override
+    public Path getConfigPath() {
+        return FMLPaths.CONFIGDIR.get();
+    }
+}
